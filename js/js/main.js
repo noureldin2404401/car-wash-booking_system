26cf9454b1e76dc5b1f4da6108690355ed96ef76
kareem2404177-dev <kareem2404177@miuegypt.edu.eg>
2026-07-26 03:17:28 +0300

    Add files via upload

---
+++ js/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var navbar = document.getElementById('main-navbar');
+    if (navbar) {
+        window.addEventListener('scroll', function () {
+            if (window.scrollY > 50) {
+                navbar.classList.add('scrolled');
+            } else {
+                navbar.classList.remove('scrolled');
+            }
+        });
+    }
+});
